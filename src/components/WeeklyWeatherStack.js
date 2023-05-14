45323e05a91a4f46a5db4e8fb2f3d73761707db0
bushinosuke <bushinosuke0129@gmail.com>
2023-05-14 16:25:48 +0900

    modified:   src/components/WeeklyWeatherStack.js

--- src/components/WeeklyWeatherStack.js
+++ src/components/WeeklyWeatherStack.js
@@ -10,14 +10,18 @@ export const WeeklyWeatherStack = (props) => {
       <Typography variant='subtitle1' bgcolor="primary.light" color="common.white" >週間予報</Typography>
       <Stack spacing={0} >
         {weeklyWeatherData.map((data,index) => {
+          const date = new Date(data.date);
+          const day = date.getDay();
+          const weekday = date.toLocaleString("ja-JP", { weekday: "long" });
+          
           return (
             <Grid container sx={{ border: 1 }} key={index}>
-              <Grid item xs={4}>{data.date}</Grid>
+              <Grid item xs={4}>{`${data.date} (${weekday.substring(0, 1)})`}</Grid>
               <Grid item xs={4}>{data.weather}</Grid>
               <Grid item xs={4}>
                 <Grid container>
-                  <Grid item xs={12}>{`最高気温：${data.tempMax}`}</Grid>
-                  <Grid item xs={12}>{`最低気温：${data.tempMin}`}</Grid>
+                  <Grid item xs={12}>{`最高：${data.tempMax}℃`}</Grid>
+                  <Grid item xs={12}>{`最低：${data.tempMin}℃`}</Grid>
                 </Grid>
               </Grid>
             </Grid>
